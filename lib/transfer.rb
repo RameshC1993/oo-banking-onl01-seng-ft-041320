28bc75b2c9eea7233f51aaa74808f01a681932eb
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-05-11 23:46:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,6 +29,9 @@ class Transfer
       @sender.deposit(-@amount)
       @receiver.deposit(@amount)
       @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end
   end
 end
