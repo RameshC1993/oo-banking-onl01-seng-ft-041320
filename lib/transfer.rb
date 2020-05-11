1bb9fbe9b7dc923952468f69783744a9122267d9
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-05-11 22:14:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer
-  attr_accessor :Transfer, :sender, :re
+  attr_accessor :transfer, :sender, :receiver 
+  
+  def initialize(transfer, sender, receiver)
+    @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+  end
 end
